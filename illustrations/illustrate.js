1017bbc603565dfd78d40e99e06b3440df92d952
namidairo777 <namidairo777@163.com>
2017-02-01 23:43:23 +0900

    js painter

---
+++ illustrations/illustrate.js
@@ -0,0 +1,52 @@
+/**
+* init function: draw 10*10 tiles
+* changeColor function: 
+* - non, black
+* - red, green
+* - lighter red, lighter green
+*
+* 
+**/
+$(document).ready(function(){
+	var canvas = $("#canvas")[0];
+	var ctx = canvas.getContext("2d");
+
+	var w = $("#canvas").width();
+	var h = $("#canvas").height();
+
+	var margin = 10
+	var map = []
+
+	/**
+	* 0: none
+	* 1: black
+	* 2: red
+	* 3: light red
+	* 4: green
+	* 5: light green
+	**/
+
+	var color = ["rgb(255, 255, 255)", "rgb(0, 0, 0)", "rgb(255, 0, 0)", "rgb(200, 0, 0)", "rgb(0, 255, 0)", "rgb(0, 200, 0)"]
+
+	function init(){
+		for(var i = 0; i < 500; i += 50){
+			var column = []
+			for(var j = 0; j < 500; j += 50){
+				column.push(0)
+			}
+			map.push(column)
+		}
+	}
+
+	$("#canvas").click(function(e){
+		x = parseInt((e.pageX - margin) / 50);
+		y = parseInt((e.pageY - margin) / 50);
+
+		map[x][y] = (map[x][y] + 1) % 6
+		ctx.fillStyle = color[map[x][y]]
+		ctx.fillRect(x*50, y*50, 50, 50);
+
+	});
+
+	init();
+});
